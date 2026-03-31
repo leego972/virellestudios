c45f8f69cee0cafee75a1bf8bd85bdc00e3441b4
leego972 <leego972@gmail.com>
2026-03-31 18:08:06 +1100

    feat: add toned-down watermark replacement component

---
+++ client/src/components/GoldWatermarkLaunch.tsx
@@ -0,0 +1,20 @@
+export default function GoldWatermarkLaunch({ className = "" }: { className?: string }) {
+  return (
+    <div className={`fixed inset-0 flex items-center justify-center pointer-events-none z-0 ${className}`}>
+      <img
+        src="https://files.manuscdn.com/user_upload_by_module/session_file/310519663418605762/hxRQQgsmyjgcByim.png"
+        alt=""
+        className="hidden dark:block w-[320px] h-[320px] sm:w-[420px] sm:h-[420px] lg:w-[520px] lg:h-[520px] object-contain opacity-[0.045]"
+        style={{ filter: "sepia(1) saturate(2.6) brightness(1.1) hue-rotate(8deg)" }}
+        draggable={false}
+      />
+      <img
+        src="https://files.manuscdn.com/user_upload_by_module/session_file/310519663418605762/hxRQQgsmyjgcByim.png"
+        alt=""
+        className="block dark:hidden w-[320px] h-[320px] sm:w-[420px] sm:h-[420px] lg:w-[520px] lg:h-[520px] object-contain opacity-[0.05]"
+        style={{ filter: "sepia(1) saturate(2.1) brightness(1.02) hue-rotate(8deg) drop-shadow(0 0 1px rgba(0,0,0,0.18))" }}
+        draggable={false}
+      />
+    </div>
+  );
+}
