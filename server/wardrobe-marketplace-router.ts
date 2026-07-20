f8ef7dbef7a277e675cb4c907b37dcd1616a7cb8
leego972 <leego972@gmail.com>
2026-07-21 04:53:48 +1000

    Connect Designer logo upload router

--- server/wardrobe-marketplace-router.ts
+++ server/wardrobe-marketplace-router.ts
@@ -1,8 +1,12 @@
+import { designerLogoRouter } from "./designer-logo-router";
 import { designerPortalRouter } from "./designer-portal-router";
 import { mergeRouters, router } from "./_core/trpc";
 import { wardrobeMarketplaceRouter as legacyWardrobeMarketplaceRouter } from "./wardrobe-marketplace-router-legacy";
 
 export const wardrobeMarketplaceRouter = mergeRouters(
   legacyWardrobeMarketplaceRouter,
-  router({ portal: designerPortalRouter }),
+  router({
+    portal: designerPortalRouter,
+    designerLogo: designerLogoRouter,
+  }),
 );
