963af6ab1ff0b74ce0ddbfc69d795f7c795615a2
leego972 <leego972@gmail.com>
2026-07-21 04:25:44 +1000

    Connect designer portal API to wardrobe marketplace router

--- server/wardrobe-marketplace-router.ts
+++ server/wardrobe-marketplace-router.ts
@@ -1,1 +1,8 @@
-export { wardrobeMarketplaceRouter } from "./wardrobe-marketplace-router-legacy";
+import { designerPortalRouter } from "./designer-portal-router";
+import { mergeRouters, router } from "./_core/trpc";
+import { wardrobeMarketplaceRouter as legacyWardrobeMarketplaceRouter } from "./wardrobe-marketplace-router-legacy";
+
+export const wardrobeMarketplaceRouter = mergeRouters(
+  legacyWardrobeMarketplaceRouter,
+  router({ portal: designerPortalRouter }),
+);
