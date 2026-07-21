ac908858f89195631c170f4f32bb6760c3ca0c2d
leego972 <leego972@gmail.com>
2026-07-22 09:49:47 +1000

    Mount additive designer commerce routers

--- server/wardrobe-marketplace-router.ts
+++ server/wardrobe-marketplace-router.ts
@@ -1,1 +1,12 @@
-export { wardrobeMarketplaceRouter } from "./wardrobe-marketplace-router-legacy";
+import { designerCommerceCheckoutRouter } from "./designer-commerce-checkout-router";
+import { designerCommerceRouter } from "./designer-commerce-router";
+import { mergeRouters, router } from "./_core/trpc";
+import { wardrobeMarketplaceRouter as legacyWardrobeMarketplaceRouter } from "./wardrobe-marketplace-router-legacy";
+
+export const wardrobeMarketplaceRouter = mergeRouters(
+  legacyWardrobeMarketplaceRouter,
+  router({
+    commerce: designerCommerceRouter,
+    commercePurchase: designerCommerceCheckoutRouter,
+  }),
+);
